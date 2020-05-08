abc940d289de14141fec81b8f77cf5545a723462
Giulia Teggi <teggi.giulia@gmail.com>
2020-05-08 23:16:19 +0100

    Trigger travis build

--- udacity-c3-frontend/src/main.ts
+++ udacity-c3-frontend/src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
+//Trigger travis build
 if (environment.production) {
   enableProdMode();
 }
