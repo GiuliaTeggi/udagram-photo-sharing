18727ff199ee5c3678891484ab98a1332d7733b8
Giulia Teggi <teggi.giulia@gmail.com>
2020-05-08 23:06:36 +0100

    Trigger travis build

--- udacity-c3-frontend/src/app/api/api.module.ts
+++ udacity-c3-frontend/src/app/api/api.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
-
+//Trigger travis build
 const components = [];
 
 @NgModule({
